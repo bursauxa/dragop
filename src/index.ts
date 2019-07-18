f2ba65464e2303c6ada970c097fec45c53c1f8ff
Antoine Bursaux <bursauxa@gmail.com>
2019-07-18 04:39:06 -0400

    Improved entry point exposure and fixed some module import issues.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { VueHelper } from './vue/VueHelper';
+import { draggable } from './directives/draggable';
+import { onDrag } from './directives/on-drag';
+import { onDrop } from './directives/on-drop';
+import { onDragAborted } from './directives/on-drag-aborted';
+import { VueConstructor } from 'vue';
 
-export default class Dragop {
-    public static addToVue() {
-        VueHelper.addDirectives();
-    }
+export function AddDragopDirectives(vue: VueConstructor) {
+    vue.directive('draggable', draggable);
+    vue.directive('onDrag', onDrag);
+    vue.directive('onDrop', onDrop);
+    vue.directive('onDragAborted', onDragAborted);
 }
+
+export const DragopDirectives = { draggable, onDrag, onDrop, onDragAborted };
--- src/vue/VueHelper.ts
+++ src/vue/VueHelper.ts
@@ -1,7 +1,3 @@
-import { draggable } from '../directives/draggable';
-import { onDrop } from '../directives/on-drop';
-import { onDrag } from '../directives/on-drag';
-import { onDragAborted } from '../directives/on-drag-aborted';
 import Vue from 'vue';
 import { VNode } from 'vue';
 
@@ -16,12 +12,5 @@ export class VueHelper {
             }
         }
         return null;
-    }
-
-    public static addDirectives(): void {
-        Vue.directive('draggable', draggable);
-        Vue.directive('on-drop', onDrop);
-        Vue.directive('on-drag', onDrag);
-        Vue.directive('on-drag-aborted', onDragAborted);
     }
 }
